5239b99fcb7a3dc55da9bd8123bd0262def3f213
Manu Phatak <bionikspoon@gmail.com>
2016-03-31 21:13:16 -0500

    add gift remote methods, add mock migration data

--- common/models/gift.js
+++ common/models/gift.js
@@ -1,3 +1,39 @@
-module.exports = function(Gift) {
+module.exports = function (Gift) {
+  /**
+   * List all available gifts
+   * @param {function(Error, array)} callback
+   */
 
+  Gift.listFree = function (callback) {
+    Gift.find({
+      fields: {
+        reserved: false
+      }
+    }, callback);
+  };
+
+  /**
+   * Check if gift is available.
+   * @param {number} id ID of gift.
+   * @param {function(Error, string, boolean)} callback
+   */
+
+  Gift.isFree = function (id, callback) {
+    Gift.findById(id, giftFoundCB);
+
+    function giftFoundCB(err, gift) {
+      if (err) return callback(err);
+      var response, available;
+      
+      if (gift.reserved) {
+        response = 'Sorry, the gift is reserved';
+        available = false;
+      }
+      else {
+        response = 'Great, this gift can be yours';
+        available = true;
+      }
+      callback(null, response, available);
+    }
+  };
 };
---
+++ server/boot/create-sample-models.js
@@ -0,0 +1,73 @@
+const async = require('async');
+const giftData = require('../../data/gifts.json');
+const receiverData = require('../../data/receivers.json');
+const donorData = require('../../data/donors.json');
+
+
+module.exports = function createSampleModels(server) {
+  const mongoDS = server.datasources.mongoDS;
+
+  // create donors and receivers;
+  async.parallel({
+    donors: async.apply(createDonors),
+    receivers: async.apply(createReceivers)
+  }, createUsersCB);
+
+  function createUsersCB(err, results) {
+    if (err) throw err;
+
+    createGifts(results.donors, results.receivers, function (err, gifts) {
+      if (err) throw err;
+
+      console.log('>', results.donors.length, 'Donor', 'models created successfully');
+      console.log('>', results.receivers.length, 'Receiver', 'models created successfully');
+      console.log('>', gifts.length, 'Gift', 'models created successfully');
+    });
+  }
+
+
+  function createDonors(callback) {
+    mongoDS.automigrate('Donor', function (err) {
+      if (err) return callback(err);
+      const Donor = server.models.Donor;
+
+      console.log('> Migrating Donors');
+
+
+      Donor.create(donorData.slice(0, 15), callback);
+    });
+  }
+
+  function createReceivers(callback) {
+    mongoDS.automigrate('Receiver', function (err) {
+      if (err) return callback(err);
+      const Receiver = server.models.Receiver;
+
+      console.log('> Migrating Receivers');
+
+      Receiver.create(receiverData.slice(0, 50), callback);
+    });
+  }
+
+  function createGifts(donors, receivers, callback) {
+    mongoDS.automigrate('Gift', function (err) {
+      if (err) return callback(err);
+      const Gift = server.models.Gift;
+
+      console.log('> Migrating Gifts');
+
+      Gift.create(giftData.map(mapGifts), callback);
+
+      function mapGifts(gift) {
+        return Object.assign({}, gift, {
+          donorId: donors[ gift.donorId ].id,
+          receiverId: receivers[ gift.receiverId ].id
+        })
+
+      }
+
+    });
+  }
+
+};
+
